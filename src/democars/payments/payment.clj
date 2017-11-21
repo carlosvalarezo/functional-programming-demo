2370950359a6b2d597895f77423e92adc311c6c5
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-11-21 03:24:57 -0500

    everything working

--- src/democars/payments/payment.clj
+++ src/democars/payments/payment.clj
@@ -2,7 +2,12 @@
   (:require [democars.rentcars.rent-operations :as operations]
             [democars.utiles.util :as util]))
 
-(defn calculateSubTotal [rentDays, car]
-  (if (= (operations/checkAvailabilityOfCar car) false)
-    (* (util/getNumberOfDays (first rentDays) (last rentDays)) (operations/getPriceByTypeOfCar car))))
 
+
+(defn calculateSubTotal [rentDates car]
+  (if (= (operations/checkAvailabilityOfCar car) false)
+    (* (util/getNumberOfDays (first rentDates) (last rentDates)) (operations/getPriceByTypeOfCar car) )))
+
+(defn calculateTotalPayment [totalDiscount totalMembership subtotal]
+  (prn totalDiscount totalMembership subtotal)
+  (+ totalDiscount totalMembership subtotal))
