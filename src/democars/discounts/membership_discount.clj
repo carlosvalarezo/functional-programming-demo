0be3e1ed805a19b979454e17d84c446c7b329b6d
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-11-21 03:22:51 -0500

    eveything working

--- src/democars/discounts/membership_discount.clj
+++ src/democars/discounts/membership_discount.clj
@@ -1,6 +1,15 @@
 (ns democars.discounts.membership-discount
-  (:require [democars.utiles.util :as util]
-            [democars.payments.payment :as payment]))
+  (:require [democars.payments.payment :as payment]
+            [democars.utiles.util :as util]))
+
+(defn getDiscount [rentDates car]
+  (util/applyDiscount (payment/calculateSubTotal rentDates car) 0.05))
+
+(defn getDiscountByMembership [calculate]
+  (fn [rentDates car]
+    (calculate rentDates car)))
+
+(def discountByMembership (getDiscountByMembership getDiscount))
 
 (defn calculateDiscountByMembership [rentDates car]
-  (util/applyDiscount (payment/calculateSubTotal rentDates car) 0.05))
+  (discountByMembership rentDates car))
