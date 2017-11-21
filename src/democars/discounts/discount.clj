b63c1076024b81e9a287ed6675f2ee6f738e87b5
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-11-21 03:22:27 -0500

    everything working

--- src/democars/discounts/discount.clj
+++ src/democars/discounts/discount.clj
@@ -1,15 +1,20 @@
 (ns democars.discounts.discount
   (:require [democars.discounts.membership-discount :as membership]
+            [democars.discounts.weekdays-discount :as weekdays]
             [democars.discounts.numberofdays-discount :as numberOfDays]
-            [democars.discounts.weekdays-discount :as weekdays]))
+            ))
 
 
-(defn calculateTotalDiscount [& args]
-  (+ args))
+(defn calculateWeekDayDiscount [rentDates car]
+  (weekdays/calculateDiscountOnWeekDays rentDates car))
 
-(defn calculateDiscount [rentDays car ]
-      (+ (weekdays/calculateDiscountOnWeekDay rentDays car)
-         (membership/calculateDiscountByMembership rentDays car)
-         (numberOfDays/calculateDiscountByNumberOfDays rentDays car)
-         ))
+(defn calculateMembershipDiscount [rentDates car]
+  (membership/calculateDiscountByMembership rentDates car))
 
+(defn calculateNumberOfDaysDiscount [rentDates car]
+  (numberOfDays/calculateDiscountByNumberOfDays rentDates car))
+
+(defn calculateTotalDiscount [rentDates car membership]
+  (+ (calculateWeekDayDiscount rentDates car)
+     (if (= membership true) (calculateMembershipDiscount rentDates car) 0))
+     (calculateNumberOfDaysDiscount rentDates car))
