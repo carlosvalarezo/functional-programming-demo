741f46a7205cd18483cc2e85cb23360c25c89aa3
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-11-21 03:23:18 -0500

    everything working

--- src/democars/discounts/numberofdays_discount.clj
+++ src/democars/discounts/numberofdays_discount.clj
@@ -6,11 +6,13 @@
             [democars.utiles.util :as util]))
 
 
+
 (defn discountByDays [amount days]
   (cond
     (and (>= days 3) (<= days 5)) (util/applyDiscount amount 0.05)
     (and (>= days 6) (<= days 10)) (util/applyDiscount amount 0.10)
-    (>= days 11) (util/applyDiscount amount 0.15)))
+    (>= days 11) (util/applyDiscount amount 0.15)
+    :else 0))
 
 (defn calculateDiscountByNumberOfDays [rentDates car]
   (discountByDays (payment/calculateSubTotal rentDates car) (util/getNumberOfDays (first rentDates) (last rentDates))))
