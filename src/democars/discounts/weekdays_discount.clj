f5c50ce73ae0fbc6736456260cb9f6829e84b5e1
Carlos Valarezo Loaiza <carlos.valarezo@gmail.com>
2017-11-21 03:23:56 -0500

    everything working

--- src/democars/discounts/weekdays_discount.clj
+++ src/democars/discounts/weekdays_discount.clj
@@ -1,14 +1,16 @@
 (ns democars.discounts.weekdays-discount
-  (:require [clj-time.core :as t]
-            [clj-time.coerce :as coerce]
-            [clj-time.local :as l]
-            [democars.rentcars.rent-operations :as operations]
+  (:require [democars.rentcars.rent-operations :as operations]
             [democars.utiles.util :as util]))
 
 (defn discountOnWeekdays [date amount]
-  (if (= true (util/isWeekday? date))
+  (if (= (util/isWeekday? date) true)
            (util/applyDiscount amount 0.10)
            (util/applyDiscount amount 0)))
 
-(defn calculateDiscountOnWeekDay [rentDates car]
-  (reduce + (map (fn [rentDate] (discountOnWeekdays rentDate (operations/getPriceByTypeOfCar car))) rentDates)))
+(defn calculateDiscountOnWeekDays [rentDates car]
+  (reduce +
+          (map
+            (fn [rentDate]
+              (discountOnWeekdays rentDate
+                                  (operations/getPriceByTypeOfCar car)))
+            rentDates)))
